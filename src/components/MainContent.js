53ed6821541b8872de19b203ca03dfe82c21e9f5
Tommy <tommypurkiss1@gmail.com>
2020-05-03 01:27:13 +0100

    skills images inputted

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,21 +3,11 @@ import React from 'react'
 
 import './MainContent.scss'
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+
 
 export default function MainContent() {
 
-    // function moveHelloTitles() {
-    //     const bottomHello = document.getElementsByClassName('bottom-hello');
-    //     const topHello = document.getElementsByClassName('top-hello');
 
-
-    // }
 
     return (
         <div className="Main-content-wrapper">
@@ -34,7 +24,6 @@ export default function MainContent() {
                     </div>
                     <div className="text-two">
                         <p>
-                        {/* I'm Tommy, a Frontend Developer based in the United Kingdom. I like to design and create beautiful websites, that perform well with a focus on responsive design.  */}
                             I also have worked with some mobile programming in Android and iOS. I'm always keen to learn new skills as a Developer.
                         </p>
                     </div>
@@ -62,14 +51,15 @@ export default function MainContent() {
                     </div>
                     <div className="project-card">
                         {/* android weather */}
-
-                        <img src={require('../androidweather.png')} alt="Android Weather"></img>
-
+                        <a href="https://play.google.com/store/apps/details?id=com.tommypurkissdev.weather" target="_blank">
+                            <img src={require('../androidweather.png')} alt="Android Weather"></img>
+                        </a>
                     </div>
-                    <div className="project-card ">
+                    <div className="project-card">
                         {/* android onlynote */}
-
-                        <img src={require('../androidonlynote.png')} alt="Android Onlynote"></img>
+                        <a href="https://play.google.com/store/apps/details?id=tommypurkissdev.simplenotes" target="_blank">
+                            <img src={require('../androidonlynote.png')} alt="Android Onlynote"></img>
+                        </a>
                     </div>
                 </div>
 
@@ -77,142 +67,71 @@ export default function MainContent() {
 
             <hr></hr>
 
-            <section className="skills-section">
+            <section className="skills-section" id="skills-section">
                 <div className="skills-title" id="skills-title">
                     <h2>What I can do</h2>
                 </div>
+                <div className="skills-container row ">
+                    <div className="skills-card">
+                        <div className="skills-img">
+                            <img src={require('../imgs/web-tech-logos/html5.svg')}></img>
+                        </div>
+                        <div className="skills-text">
+                            HTML5
+                        </div>
+                    </div>
+
+                    <div className="skills-card">
+                        <div className="skills-img">
+                            <img src={require('../imgs/web-tech-logos/css-5.svg')}></img>
+
+                        </div>
+                        <div className="skills-text">
+                            CSS3 / Sass
+                        </div>
+                    </div>
+
+                    <div className="skills-card">
+                        <div className="skills-img">
+                            <img src={require('../imgs/web-tech-logos/javascript-4.svg')}></img>
+                        </div>
+                        <div className="skills-text">
+                            Javascript / jQuery
+                        </div>
+                    </div>
+
+                    <div className="skills-card">
+                        <div className="skills-img">
+                            <img src={require('../imgs/web-tech-logos/react-2.svg')}></img>
+                        </div>
+                        <div className="skills-text">
+                            ReactJS
+                        </div>
+                    </div>
+
+                    <div className="skills-card">
+                        <div className="skills-img">
+                            <img src={require('../imgs/web-tech-logos/firebase-1.svg')}></img>
+                        </div>
+                        <div className="skills-text">
+                            Firebase
+                        </div>
+                    </div>
+
+                    <div className="skills-card">
+                        <div className="skills-img">
+                            <img src={require('../imgs/web-tech-logos/adobe-xd-1.svg')}></img>
+                        </div>
+                        <div className="skills-text">
+                            Adobe XD
+                        </div>
+                    </div>
+                </div>
             </section>
 
-            {/* <div className="welcome-section">
-                <h1 className="h1-one">
-                    hello.
-                </h1>
-                <h1 className="h1-two top-hello">
-                    hello.
-                </h1>
-                <h1 className="h1-three bottom-hello">
-                    hello.
-                </h1>
-                <div className="welcome-text">
-                    <div className="text-one">
-                        <p>
-
-                        </p>
-                    </div>
-                    <div className="text-two">
-                        <p>
-
-                        </p>
-                    </div>
-                </div>
-            </div> */}
-
-
-
-            {/* <div className="project-section">
-                <h2 id="projects-title">Projects</h2>
-
-                <Router>
-                <div className="project-types-titles">
-
-                    <Link to="/all" className="proj-link">All</Link>
-
-                    <Link to="/web" className="proj-link">Web</Link>
-
-                    <Link to="/mobile" className="proj-link">Mobile</Link>
-
-                </div>
-                
-                <Switch>
-                <div className="projects-container" >
-                    <Route exact path="/" render={() => (
-                        <AllComponent />
-                    )} />
-                    <Route path="/all">
-                        <AllComponent />
-                    </Route>
-                    <Route path="/web">
-                        <WebComponent />
-                    </Route>
-                    <Route path="/mobile">
-                        <MobileComponent />
-                    </Route>
-                </div> 
-                </Switch>
-
-                </Router>
-
-            </div>
-
-            <div className="about-section">
-
-            </div> */}
         </div>
     )
 }
 
 
-
-
-function AllComponent() {
-    return(
-        <div>
-
-            <div className="projects-container">
-
-                <div className="test web ">
-                    web
-                    </div>
-                <div className="test web ">
-                    web two
-                </div>
-                <div className="test android ">
-                    android
-                </div>
-                <div className="test android ">
-                    android two
-                </div>
-                {/* ADD NEW PROJECTS AS NEEDED */}
-            </div>
-        </div>
-    );
-}
-
-function WebComponent() {
-    return(
-        <div>
-
-            <div className="projects-container">
-
-                <div className="test web ">
-                    web
-                </div>
-                <div className="test web ">
-                    web two
-                </div>
-                {/* ADD NEW PROJECTS AS NEEDED */}
-
-            </div>
-        </div>
-    );
-}
-
-function MobileComponent() {
-    return(
-        <div>
-
-            <div className="projects-container">
-
-                <div className="test android ">
-                    android
-                </div>
-                <div className="test android ">
-                    android two
-                </div>
-                {/* ADD NEW PROJECTS AS NEEDED */}
-
-            </div>
-        </div>
-    );
-}
 
